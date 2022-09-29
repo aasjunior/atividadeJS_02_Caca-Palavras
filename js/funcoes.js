2ff62bb94c6a8146345928b9c6ab0bd5c9310851
fatec <fatec@fatec.com>
2022-09-29 19:14:43 -0300

    1.5 function colorirPalavra() -> coloca border na palavra

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,13 +1,15 @@
+/* Lê o XML */  
 xmlhttp = new XMLHttpRequest();//Instanciação do objeto 
 xmlhttp.open("GET", "xml/lista_animes.xml", false);
 xmlhttp.send();
 xmlDoc = xmlhttp.responseXML;//Informa que o tipo de arquivo é XML
 let animes = xmlDoc.getElementsByTagName("anime");//let - variavel para conjunto de dados
+var cont = animes.length;//Armazena a quantidade de indices dentro do vetor animes
 
 matriz = new Array;
 x = 0;
 
-/*16x22 */
+//MATRIZ DO CAÇA PALAVRAS 16 LINHAS x 22 COLUNAS
 matriz = [
     ['N','M','H','D','H','E','H','E','T','B','C','O','W','B','O','Y','B','E','B','O','P','A'],
     ['I','S','E','R','I','A','L','E','X','P','E','R','I','M','E','N','T','S','L','A','I','N'],
@@ -27,22 +29,27 @@ matriz = [
     ['C','N','O','I','L','E','G','N','A','V','E','S','I','S','E','N','E','G','N','O','E','N']
 ];
 
+//EXIBIÇÃO DOS VALORES DA MATRIZ DENTRO DE UMA <TABLE>
 document.write("<table>");
 for(linha=0; linha<16; linha++){
+    //CRIA UMA NOVA LINHA <TR> A CADA ITERAÇÃO
     document.write("<tr>");
     for(coluna=0; coluna<22; coluna++){
+        /*  CRIA UMA NOVA COLUNA <TD> DENTRO DA LINHA A CADA ITERAÇÃO
+            X CORRESPONDE AO ID DE CADA <TD> 
+        */
         x++;
         document.write("<td id='" + x + "' class='"+ x +"' onclick='funcSelect("+x+")' align='center' >" + matriz[linha][coluna] + "</td>");
     }
     document.write("</tr>");
-    console.log(x);
 }
 document.write("</table>");
 
-
+//NO FINAL DO LAÇO DE REPETIÇÃO É CHAMADA A FUNÇÃO DE COLORIR A PALAVRA
+colorirPalavra();
 
 function funcSelect(x){
-    document.getElementById(x).style.color = "1px solid #000";
+    document.getElementById(x).style.border = "1px solid #000";
     letra = document.getElementsByClassName(x)[0].childNodes[0].nodeValue;
     document.getElementById('teste').innerHTML += letra;
     console.log(x);
@@ -50,8 +57,23 @@ function funcSelect(x){
 }
 
 function colorirPalavra(){
-    for(i=0; i <= animes.length-1; i++){
-        ini = animes[i].getElementsByTagName('inicial')[0].childNodes[0].nodeValue;
-        console.log(ini);
+    for(i=0; i<=cont-1; i++){
+        /*
+            parseInt() -> função que converte string para inteiro
+            inicial -> recebe o id da primeira letra da palavra armazenada na tag <inicial></inicial> do xml
+            final -> recebe o id da ultima letra da palavra armazenada na tag <final></final> do xml
+            incremento -> recebe o valor armazenada na tag <incremento></incremento> do xml
+        */
+        inicial = parseInt(animes[i].getElementsByTagName('inicial')[0].childNodes[0].nodeValue);
+        final = parseInt(animes[i].getElementsByTagName('final')[0].childNodes[0].nodeValue);
+        incremento = parseInt(animes[i].getElementsByTagName('incremento')[0].childNodes[0].nodeValue);
+        console.log("inicial: " + inicial);
+        console.log("final: " + final);
+        console.log("incremento: " + incremento);
+
+        for(z=inicial; z<=final; z=z+incremento){
+            console.log(z);
+            document.getElementById(z).style.border = "1px solid #000";
+        }
     }
 }
